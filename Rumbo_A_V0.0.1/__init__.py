c1d8ddf9f50116f6deb5c6c2d77bbc8ae2d73b54
IngoBongo <ingobongo@gmail.com>
2017-10-04 21:19:47 +0200

    archivos init y main versión 0.0.1 añadidos

---
+++ Rumbo_A_V0.0.1/__init__.py
@@ -0,0 +1,7 @@
+"""
+{Rumbo A... pretende ser un juego textual de aventuras}
+Copyright (C) {2017}  {by Igor Iglesia Gonzalez}
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+"""
